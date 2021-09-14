3ac6d2e7f0ce26889386928de477f33434e02a07
Kevin Cruz <kocruz.24@gmail.com>
2021-09-14 05:06:36 -0400

    Implement post_drinks() route

--- backend/src/api.py
+++ backend/src/api.py
@@ -93,17 +93,47 @@ def get_drinks_detail(jwt):
 
 
 # @TODO implement endpoint
-#     POST /drinks
+#     POST /drinks (DONE)
 '''
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
+    returns status code 200 and json {"success": True, "drinks": drink} 
+    where drink an array containing only the newly created drink
+    or appropriate status code indicating reason for failure
 '''
 
+
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def post_drinks(jwt):
+    show_auth_user(jwt)
+
+    try:
+        body = request.get_json()
+
+        get_title = body.get("title", None)
+        # Check if a drink already exists with this title
+        if Drink.query.filter_by(title=get_title).first():
+            abort(422)
+
+        get_recipe = json.dumps(body.get('recipe', None))
+
+        new_drink = Drink(title=get_title, recipe=get_recipe)
+        new_drink.insert()
+
+        long_new_drink = new_drink.long()
+
+        return jsonify({
+            'success': True,
+            'drink': long_new_drink
+        })
+    except():
+        abort(422)
+
+
 # @TODO implement endpoint
-#     PATCH /drinks/<id>
+#     PATCH /drinks/<id> (DONE)
 '''
         where <id> is the existing model id
         it should respond with a 404 error if <id> is not found
